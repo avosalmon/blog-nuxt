9574597030ee50d39797c606dcf3770d2b90e1c1
avosalmon <avosalmon@gmail.com>
2021-03-06 14:11:05 +0800

    uninstall @nuxtjs/axios

--- nuxt.config.js
+++ nuxt.config.js
@@ -39,13 +39,7 @@ export default {
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-    // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios',
-  ],
-
-  // Axios module configuration: https://go.nuxtjs.dev/config-axios
-  axios: {},
+  modules: [],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
