9b875ff74aabb5bb5325670acae828a6d17c8877
avosalmon <avosalmon@gmail.com>
2021-03-06 21:21:16 +0800

    main page template

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,57 @@
 <template>
-  <div>content</div>
+  <div>
+    <article v-for="post in posts" :key="post.id" class="mb-10">
+      <NuxtLink class="flex flex-col md:flex-row" :to="`/posts/${post.slug}`">
+        <img
+          class="object-cover mb-2 md:h-48 md:w-48 md:mr-10"
+          :src="post.featured_image ? post.featured_image.url : ''"
+          :alt="post.title"
+        />
+        <div>
+          <h1 class="text-2xl font-semibold leading-tight mb-2">
+            {{ post.title }}
+          </h1>
+          <p class="text-gray-600 mb-4">{{ post.published_at }}</p>
+          <p class="leading-relaxed hidden md:block">{{ post.excerpt }}</p>
+        </div>
+      </NuxtLink>
+    </article>
+    <nav>
+      <ul class="flex justify-between">
+        <li :class="{ invisible: isFirstPage }">
+          <NuxtLink :to="'/posts/page/${previousPage}'" class="underline"
+            >« Previous</NuxtLink
+          >
+        </li>
+        <li :class="{ invisible: isLastPage }">
+          <NuxtLink :to="'/posts/page/${nextPage}'" class="underline"
+            >Next »</NuxtLink
+          >
+        </li>
+      </ul>
+    </nav>
+  </div>
 </template>
 
 <script>
-export default {}
+export default {
+  async asyncData({ $http }) {
+    const { data, meta } = await $http.$get('/api/posts')
+    return { posts: data, meta }
+  },
+  computed: {
+    isFirstPage() {
+      return this.meta.current_page === 1
+    },
+    isLastPage() {
+      return this.meta.current_page === this.meta.last_page
+    },
+    previousPage() {
+      return this.meta.current_page - 1
+    },
+    nextPage() {
+      return this.meta.current_page + 1
+    },
+  },
+}
 </script>
